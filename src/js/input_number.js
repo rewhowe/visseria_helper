ad124f157caae4e878f55883d44639553bc98a56
andrew.howe <andrew.howe@rakuten.com>
2018-10-18 12:15:26 +0900

    move buttons to html; add some styling

--- src/js/input_number.js
+++ src/js/input_number.js
@@ -1,19 +1,27 @@
 $(function () {
-  // TODO: move to html
-  // hide
-  // show on focus
-  // hide on focusout
+  // TODO:
   // add click handler
-  // add position absolute to css
-  // add more css
-  const $upButton = $('<i class="up-button ">▲</i>');
-  const $downButton = $('<i class="down-button ">▼</i>');
 
-  $(document.body).append($upButton).append($downButton);
+  const MARGIN = 5; // px
+
+  const $upButton = $('.js-up-button');
+  const $downButton = $('.js-down-button');
 
   $(document).on('focus', 'input[type="number"]', function () {
     let rect = this.getBoundingClientRect();
-    $upButton.css({ position: 'absolute', top: rect.y + 'px', left: rect.right + 'px' });
-    $downButton.css({ position: 'absolute', top: rect.y + 'px', left: rect.left - $downButton.width() + 'px' });
+    $upButton.removeClass('hidden')
+      .css({
+        top: rect.y + window.pageYOffset + 'px',
+        left: rect.right + window.pageXOffset + MARGIN + 'px'
+      });
+    $downButton.removeClass('hidden')
+      .css({
+        top: rect.y + window.pageYOffset + 'px',
+        left: rect.left - $downButton.width() + window.pageXOffset - MARGIN + 'px'
+      });
+  })
+  .on('focusout', 'input[type="number"]', function () {
+    $upButton.addClass('hidden');
+    $downButton.addClass('hidden');
   });
 });
