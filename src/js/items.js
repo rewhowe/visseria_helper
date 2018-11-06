8eebbadfda85e6b5d86f0fad37ba23fe66335371
andrew.howe <andrew.howe@rakuten.com>
2018-11-06 11:47:34 +0900

    add item list

---
+++ src/js/items.js
@@ -0,0 +1,71 @@
+// @require Select
+
+const Items = {};
+
+Items.ITEMS = [
+  {
+    name: 'Antidote',
+    cost: 3,
+    effect: 'Removes all Debuffs from 1 User',
+  },
+  {
+    name: 'Bangbang Bomb',
+    cost: 3,
+    effect: 'Deal 5 DMG to 1 Enemy ',
+  },
+  {
+    name: 'Counterfeit Coin',
+    cost: 3,
+    effect: 'This Item is treated as 1G',
+  },
+  {
+    name: 'Elixir',
+    cost: 3,
+    effect: 'Recharges the User\'s Ultimate Gauge 1',
+  },
+  {
+    name: 'Liquid Courage',
+    cost: 3,
+    effect: 'Grants +2 Decisive roll for the Room',
+  },
+  {
+    name: 'Lucky Fruit',
+    cost: 3,
+    effect: 'User gains +2 Success roll for the Room',
+  },
+  {
+    name: 'Minimimic',
+    cost: 3,
+    effect: 'Immediately battle a Mimic Mob',
+  },
+  {
+    name: 'Potion',
+    cost: 3,
+    effect: '1 active User Recovers (Room Lvl) +5 HP',
+  },
+  {
+    name: 'Resurrection Ring',
+    cost: 15,
+    effect: 'Resurrects 1 Dead User to Max HP with no Debuffs',
+  },
+  {
+    name: 'Shine Shield',
+    cost: 3,
+    effect: 'Blocks 5 DMG',
+  },
+  {
+    name: 'Smoke Bomb',
+    cost: 3,
+    effect: 'All active Users can Escape the active Room regardless of Success roll and return to the previous Room. Escaped Room Tile is reset',
+  },
+  {
+    name: 'Sorcerer\'s Eye',
+    cost: 3,
+    effect: 'Reveals all adjacent Room Tiles. Any Room not entered by the end of the turn is reset',
+  },
+  {
+    name: 'Stamina Potion',
+    cost: 3,
+    effect: '1 inactive User becomes active',
+  },
+];
